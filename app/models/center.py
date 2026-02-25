77c8902f4bd67167f76e05221df9cb56608c2c63
amrsamida <amr.samida@gmail.com>
2026-02-25 00:05:37 +0000

    Finalize centers models and routers

--- app/models/center.py
+++ app/models/center.py
@@ -6,6 +6,11 @@ class Center(BaseModel):
     city: str
 
 
+class CenterCreate(BaseModel):
+    name: str
+    city: str
+
+
 class CenterUpdate(BaseModel):
     name: str | None = None
     city: str | None = None
